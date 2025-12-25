2fae47761bc1b7e384f2e08b8d7024b3b46df8f4
Abissens <hichem.abissens@gmail.com>
2025-12-25 15:44:32 +0100

    Initial commit

---
+++ src/main/java/tech/elethoughts/courses/cloud/feign/SpringCloudOpenfeignConfigApplication.java
@@ -0,0 +1,15 @@
+package tech.elethoughts.courses.cloud.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class SpringCloudOpenfeignConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudOpenfeignConfigApplication.class, args);
+    }
+
+}
---
+++ src/main/java/tech/elethoughts/courses/cloud/feign/domain/Pokemon.java
@@ -0,0 +1,4 @@
+package tech.elethoughts.courses.cloud.feign.domain;
+
+public record Pokemon(Long id, String name, int height, int weight) {
+}
